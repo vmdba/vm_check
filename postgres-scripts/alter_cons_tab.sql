64e1720a5f8ba1ecc667f0596fbfd54c5f9bee65
Database Administrator <codesql@users.noreply.github.com>
2016-10-20 17:59:56 -0600

    Update alter_cons_tab.sql

--- postgres-scripts/alter_cons_tab.sql
+++ postgres-scripts/alter_cons_tab.sql
@@ -1,1 +1,7 @@
+
+
 SELECT 'alter table ' || conrelid::regclass ||  ' add constraint ' || conname ||  ' ' || pg_get_constraintdef(c.oid) || ';'   FROM  pg_constraint c JOIN pg_namespace n ON n.oid = c.connamespace;
+
+
+
+
