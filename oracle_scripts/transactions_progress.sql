11a86406ebd4a79b0e29b686f03735e019d2ae6f
vmdba <newsliew@gmail.com>
2016-11-16 21:35:33 -0700

    Delete transactions_progress.sql

--- oracle_scripts/transactions_progress.sql
+++
@@ -1,5 +0,0 @@
-set pages 9999 lines 200
-col sess_id for a15
-col message for a80
-col pct_comp for 999999999
-select  inst_id|| ':' || sid || ',' || serial# sess_id, sofar, totalwork, 100-round(( 1-(sofar/totalwork))* 100) pct_comp, message from gv$session_longops where sofar < totalwork;
