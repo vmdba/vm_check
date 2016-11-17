e5634d1bf70798463bc74d3f5f9199a3a6c964b3
vmdba <newsliew@gmail.com>
2016-11-16 21:36:54 -0700

    Delete alter_cons_tab.sql

--- postgres-scripts/alter_cons_tab.sql
+++
@@ -1,7 +0,0 @@
-
-
-SELECT 'alter table ' || conrelid::regclass ||  ' add constraint ' || conname ||  ' ' || pg_get_constraintdef(c.oid) || ';'   FROM  pg_constraint c JOIN pg_namespace n ON n.oid = c.connamespace;
-
-
-
-
